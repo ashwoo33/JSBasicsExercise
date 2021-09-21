ec43e2b386dbf45a4bb561bebadb71e00eff959f
Ashley Wooten <ashley.cummings.33.ac@gmail.com>
2021-09-21 09:52:54 -0500

    JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,53 @@
+// JS BASICS EXERCISE
+// 1
+console.log(14);
+
+// 2
+// Numbers, Strings, Booleans, Null, & Underfined
+
+// 3
+// Addition is] +, Subtraction is -, Multiplication *, Division is /, Exponentiation is **, Modulus is %
+
+// 4
+console.log(5%4);
+console.log(5%2);
+console.log(1%5);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Matt".length + 'Riley'.length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length % 4);
+// console.log("I".length % "Am".length - "Iron".length % "Man".length);
+// console.log("I".length + "Am".length * "Iron".length / "Man".length);
+
+// 4
+console.table("CAP"[1]);
+console.table("VISION"[0]);
+console.table("OKOYE"[4]);
+console.table("NICK FURY"[0]);
+console.table("GROOT"[0]);
+console.table("DOCTOR STRANGE"[13]);
+console.table("THOR"[3]);
+console.table("STAR-LORD"[0]);
+
+// To complete bonus question using only 1 console log
+// console.log(CAP[1] + "\n" + "VISION"[0] + "\n" + "OKOYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0])
+
+// console.log(CAP[1], "VISION"[0], "OKOYE"[4], "NICK FURY"[0], "GROOT"[0], "DOCTOR STRANGE"[13], "THOR"[3], "STAR-LORD"[0]])
